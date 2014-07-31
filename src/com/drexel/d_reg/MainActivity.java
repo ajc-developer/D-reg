c3ce1af64a4b1ccc90fd093bc04b5783d8312eb9
ajc-developer <ajc.developer@gmail.com>
2014-07-30 21:01:43 -0400

    Update to main, added login

--- src/com/drexel/d_reg/MainActivity.java
+++ src/com/drexel/d_reg/MainActivity.java
@@ -3,11 +3,15 @@ package com.drexel.d_reg;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -26,15 +30,48 @@ public class MainActivity extends Activity {
         final ListView list = (ListView)findViewById(R.id.listView1);
         final CArrayAdapter a = new CArrayAdapter(this, classArray);
         list.setAdapter(a);
+        list.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+					final int pos, long arg3) {
+				DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+				    @Override
+				    public void onClick(DialogInterface dialog, int which) {
+				        switch (which){
+				        case DialogInterface.BUTTON_POSITIVE:
+				        	classArray.remove(pos);
+				        	a.setListData(classArray);
+							a.notifyDataSetChanged();
+				            break;
+
+				        case DialogInterface.BUTTON_NEGATIVE:
+				            
+				            break;
+				        }
+				    }
+				};
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+				builder.setMessage("Are you sure you want to remove this item?").setPositiveButton("Yes", dialogClickListener)
+				    .setNegativeButton("No", dialogClickListener).show();
+				return false;
+			}
+		});
         add.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if ("".equals(crnEditText.getText().toString()) || "".equals(nameEditText.getText().toString())){
-					System.out.println("No.");
-				}else{
-					System.out.println("yes");
-					Class c = new Class(crnEditText.getText().toString(), nameEditText.getText().toString());
-					classArray.add(c);
+				if ("".equals(nameEditText.getText().toString())) {
+					nameEditText.setError(getString(R.string.error_field_required));
+				} else{
+					nameEditText.setError(null);
+				}
+				if ("".equals(crnEditText.getText().toString())) {
+					crnEditText.setError(getString(R.string.error_field_required));	
+				} else {
+					crnEditText.setError(null);
+				}
+				if (!("".equals(crnEditText.getText().toString()) || "".equals(nameEditText.getText().toString()))){
+					classArray.add(new Class(crnEditText.getText().toString(), nameEditText.getText().toString()));
 					a.setListData(classArray);
 					a.notifyDataSetChanged();
 				}
